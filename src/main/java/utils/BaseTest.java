8d439681dea7e0e806358a63f95f3b0ef06afd75
Dragos Tanta <dragos.tanta@gmail.com>
2023-10-19 21:41:39 +0300

    curs14

---
+++ src/main/java/utils/BaseTest.java
@@ -0,0 +1,30 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+
+public class BaseTest {
+
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();//maximizes the browser window
+		driver.get("https://keybooks.ro");//navigates to the specified url
+		
+	}
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException  {
+		Thread.sleep(4000);//bad practice
+		//driver.close(); // inchide tabul curent
+		driver.quit();// inchide browserul indiferent cate taburi sunt deschide
+	}
+	
+	
+	
+}
