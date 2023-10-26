a30c8b0bd017ced89554eeb9ae7b5c87d3ee8cba
Dragos Tanta <dragos.tanta@gmail.com>
2023-10-26 21:29:36 +0300

    curs16 final

---
+++ src/main/java/tests/ImplictWaitExample.java
@@ -0,0 +1,31 @@
+package tests;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import utils.BaseTest;
+
+public class ImplictWaitExample extends BaseTest {
+
+	
+	@Test
+	public void implicitWait() {
+		
+		driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.findElement(By.cssSelector("div[id='start']>button")).click();
+		
+		WebElement finish =  driver.findElement(By.id("finish"));
+		assertEquals(finish.getText(), "Hello World!");
+		
+	}
+	
+	
+}
